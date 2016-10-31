9ec4ecc0b8200828b41ce9ed13716b53d86d0f59
Samantha Davis <samantha.shea.davis@gmail.com>
2016-10-30 23:18:56 -0400

    groundwork laid for filtering

--- src/js/controllers/listController.js
+++ src/js/controllers/listController.js
@@ -1,10 +1,50 @@
-angular.module('todoApp').controller('ListController', function(localStorageService) {
+angular.module('todoApp').controller('ListController', function(localStorageService, $stateParams) {
 
-  this.getTodo = function() {
-    return localStorageService.get('localStorageTodo') || [];
-  };
+    this.getTodo = function() {
+        return localStorageService.get('localStorageTodo') || [];
+    };
 
-  this.message = 'in listController';
-  this.todoItems = this.getTodo();
-  
+    this.setTodo = function(todoArray) {
+        localStorageService.set('localStorageTodo', todoArray);
+    };
+
+    this.removeTodo = function(todo) {
+        return localStorageService.remove('localStorageTodo', todo);
+    };
+
+    this.todoItems = this.getTodo();
+
+    this.findTodoById = function(todoArray, id) {
+        var todoToReturn;
+        todoArray.forEach(function(item) {
+            if (item.id === id) {
+                todoToReturn = item;
+            }
+        });
+        return todoToReturn;
+    };
+
+    this.toggleChecked = function(item) {
+        item.checked = !item.checked;
+        var todoArray = this.getTodo();
+        var todoToUpdate = this.findTodoById(todoArray, item.id);
+        angular.copy(item, todoToUpdate);
+        this.setTodo(todoArray);
+    };
+
+this.listFilter = $stateParams.filter || 'all';
+this.filtersObj = {
+        all: {},
+        completed: {
+            checked: true
+        },
+        active: {
+            checked: false
+        }
+    };
+
+    this.delete = function(item) {
+      console.log(this.filtersObj[this.listFilter]);
+    };
+
 });
